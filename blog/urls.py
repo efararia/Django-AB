970bbed1d67d8d5d0b03cef02512d431def4770c
erfan <efararia@gmail.com>
2026-02-08 03:36:06 +0330

    chapter 7 completed

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.blog_view, name='index'),
     path('<int:pid>/', views.blog_single, name='single'),
+    path('category/<str:cat_name>', views.blog_view, name='category'),
+    path('author/<str:author_username>', views.blog_view, name='author'),
+    path('search/', views.blog_search, name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,34 @@
+from unicodedata import category
+from django.core import paginator
 from django.shortcuts import render, get_object_or_404
+from django.template import context
 from blog.models import Post
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 # Create your views here.
 
-def blog_view(request):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=1)
+
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name = kwargs['cat_name'])
+
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username = kwargs['author_username'])
+
+    posts = Paginator(posts, 3)
+
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+    except Post.DoesNotExist:
+        posts = posts.get_page(1)
+
+
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -12,4 +37,14 @@ def blog_single(request, pid):
     post = get_object_or_404(posts, pk=pid)
     context = {'post':post}
     return render(request, 'blog/blog-single.html', context)
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method =='GET':
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains = s)
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html', context)
+
+
 
